855fe7b0e343026f73469938f134a6c8e4863706
OmAmar <bt23cse106@iiitn.ac.in>
2025-08-13 02:29:57 +0530

    updated

---
+++ Range Queries/Subarray_Sum_Queries_II.py
@@ -0,0 +1,127 @@
+import sys,math,cmath,random,os
+from heapq import heappush,heappop
+from bisect import bisect_right,bisect_left
+from collections import Counter,deque,defaultdict
+from itertools import permutations,combinations
+from io import BytesIO, IOBase
+from decimal import Decimal,getcontext
+
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+# functions #
+# MOD = 998244353
+MOD = 10**9 + 7
+RANDOM = random.randrange(1,2**62)
+def gcd(a,b):
+    while b:
+        a,b = b,a%b
+    return a
+def lcm(a,b):
+    return a//gcd(a,b)*b
+def w(x):
+    return x ^ RANDOM
+II = lambda : int(sys.stdin.readline().strip())
+LII = lambda : list(map(int, sys.stdin.readline().split()))
+MI = lambda x : x(map(int, sys.stdin.readline().split()))
+SI = lambda : sys.stdin.readline().strip()
+SLI = lambda : list(map(lambda x:ord(x)-97,sys.stdin.readline().strip()))
+LII_1 = lambda : list(map(lambda x:int(x)-1, sys.stdin.readline().split()))
+LII_C = lambda x : list(map(x, sys.stdin.readline().split()))
+MATI = lambda x : [list(map(int, sys.stdin.readline().split())) for _ in range(x)]
+##
+
+#String hashing: shclass, fenwick sortedlist: fsortl, Number: numtheory/numrare, SparseTable: SparseTable
+#Bucket Sorted list: bsortl, Segment Tree(lp,selfop): SegmentTree, bootstrap: bootstrap, Trie: tries
+#binary indexed tree: BIT, Segment Tree(point updates): SegmentPoint, Convex Hull: hull, BitArray: bitarray
+#Combinatorics: pnc, Diophantine Equations: dpheq, DSU: DSU, Geometry: Geometry, FFT: fft, XOR_dict: xdict
+#Persistent Segment Tree: perseg, Binary Trie: b_trie, HLD: hld, String funcs: sf, Segment Tree(lp): SegmentOther
+#Graph1(dnc,bl): graphadv, Graph2(khn,sat): 2sat, Graph3(fltn,bprt): graphflatten, Graph4(ep,tp,fw,bmf): graphoth
+#Graph5(djik,bfs,dfs): graph, Graph6(dfsin): dfsin, utils: utils, Persistent DSU: perdsu, Merge Sort Tree: sorttree
+#2-D BIT: 2DBIT, MonoDeque: mono, nummat: matrix, SuffixAutomaton: sautomaton, linalg: linalg, SquareRtDecomp: sqrt
+#Template : https://github.com/OmAmar106/Template-for-Competetive-Programming
+# input_file = open(r'input.txt', 'r');sys.stdin = input_file
+
+class SegmentTree:
+    @staticmethod
+    def func(left, right):
+        return (min(left[0], right[0]),max(left[1], right[1]),max(left[2], right[2], right[1] - left[0]))
+    def __init__(self, arr):
+        arr = [0] + arr
+        for i in range(1, len(arr)):
+            arr[i] += arr[i - 1]
+        self.n1 = len(arr) - 1
+        self.n = (1 << ((len(arr) - 1).bit_length()))
+        self.tree = [(float('inf'), -float('inf'), -float('inf'))] * (2 * self.n)
+        for i in range(len(arr)):
+            self.tree[self.n + i] = (arr[i], arr[i], 0)
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = self.func(self.tree[2 * i], self.tree[2 * i + 1])
+    def query(self, l, r):
+        """Query range [l, r] inclusive."""
+        l += self.n
+        r += self.n
+        res_left = (float('inf'), -float('inf'), -float('inf'))
+        res_right = (float('inf'), -float('inf'), -float('inf'))
+        while l <= r:
+            if l % 2 == 1:
+                res_left = self.func(res_left, self.tree[l])
+                l += 1
+            if r % 2 == 0:
+                res_right = self.func(self.tree[r], res_right)
+                r -= 1
+            l //= 2
+            r //= 2
+        return self.func(res_left, res_right)
+
+
+def solve():
+    n,m = LII()
+    L = LII()
+
+    seg = SegmentTree(L)
+
+    for _ in range(m):
+        l,r = LII_1()
+        print(seg.query(l,r+1)[2])
+
+    #L1 = LII()
+    #st = SI()
+solve()
