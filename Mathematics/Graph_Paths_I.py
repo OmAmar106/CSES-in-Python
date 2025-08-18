72bdc15a5d47c66aac739b3a95b7fb9c6db2b0cf
OmAmar <bt23cse106@iiitn.ac.in>
2025-08-19 01:27:35 +0530

    updated

---
+++ Mathematics/Graph_Paths_I.py
@@ -0,0 +1,183 @@
+import sys,math,cmath,random,os
+from heapq import heappush,heappop
+from bisect import bisect_right,bisect_left
+from collections import Counter,deque,defaultdict
+from itertools import permutations,combinations
+from io import BytesIO, IOBase
+from decimal import Decimal,getcontext
+
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._file = file
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+# functions #
+# MOD = 998244353
+MOD = 10**9 + 7
+RANDOM = random.randrange(1,2**62)
+def gcd(a,b):
+    while b:
+        a,b = b,a%b
+    return a
+def lcm(a,b):
+    return a//gcd(a,b)*b
+def w(x):
+    return x ^ RANDOM
+II = lambda : int(sys.stdin.readline().strip())
+LII = lambda : list(map(int, sys.stdin.readline().split()))
+MI = lambda x : x(map(int, sys.stdin.readline().split()))
+SI = lambda : sys.stdin.readline().strip()
+SLI = lambda : list(map(lambda x:ord(x)-97,sys.stdin.readline().strip()))
+LII_1 = lambda : list(map(lambda x:int(x)-1, sys.stdin.readline().split()))
+LII_C = lambda x : list(map(x, sys.stdin.readline().split()))
+MATI = lambda x : [list(map(int, sys.stdin.readline().split())) for _ in range(x)]
+##
+
+#String hashing: shclass, fenwick sortedlist: fsortl, Number: numtheory/numrare, SparseTable: SparseTable
+#Bucket Sorted list: bsortl, Segment Tree(lp,selfop): SegmentTree, bootstrap: bootstrap, Trie: tries
+#binary indexed tree: BIT, Segment Tree(point updates): SegmentPoint, Convex Hull: hull, BitArray: bitarray
+#Combinatorics: pnc, Diophantine Equations: dpheq, DSU: DSU, Geometry: Geometry, FFT: fft, XOR_dict: xdict
+#Persistent Segment Tree: perseg, Binary Trie: b_trie, HLD: hld, String funcs: sf, Segment Tree(lp): SegmentOther
+#Graph1(dnc,bl): graphadv, Graph2(khn,sat): 2sat, Graph3(fltn,bprt): graphflatten, Graph4(ep,tp,fw,bmf): graphoth
+#Graph5(djik,bfs,dfs): graph, Graph6(dfsin): dfsin, utils: utils, Persistent DSU: perdsu, Merge Sort Tree: sorttree
+#2-D BIT: 2DBIT, MonoDeque: mono, nummat: matrix, SuffixAutomaton: sautomaton, linalg: linalg, SquareRtDecomp: sqrt
+#Template : https://github.com/OmAmar106/Template-for-Competetive-Programming
+# input_file = open(r'input.txt', 'r');sys.stdin = input_file
+
+def extras():
+    getcontext().prec = 50
+    sys.setrecursionlimit(10**6)
+    sys.set_int_max_str_digits(10**5)
+# extras()
+
+def interactive():
+    import builtins
+    # print(globals())
+    globals()['print'] = lambda *args, **kwargs: builtins.print(*args, flush=True, **kwargs)
+# interactive()
+
+def GI(n,m=None,sub=-1,dirs=False,weight=False):
+    if m==None:
+        m = n-1
+    d = [[] for i in range(n)]
+    if not weight:
+        for i in range(m):
+            u,v = LII_C(lambda x:int(x)+sub)
+            d[u].append(v)
+            if not dirs:
+                d[v].append(u)
+    else:
+        for i in range(m):
+            u,v,w = LII()
+            d[u+sub].append((v+sub,w))
+            if not dirs:
+                d[v+sub].append((u+sub,w))
+    return d
+
+ordalp = lambda s : ord(s)-65 if s.isupper() else ord(s)-97
+alp = lambda x : chr(97+x)
+yes = lambda : print("Yes")
+no = lambda : print("No")
+yn = lambda flag : print("Yes" if flag else "No")
+printf = lambda x : print(-1 if x==float('inf') else x)
+lalp = 'abcdefghijklmnopqrstuvwxyz'
+ualp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+dirs = ((1,0),(0,1),(-1,0),(0,-1))
+dirs8 = ((1,0),(0,1),(-1,0),(0,-1),(1,-1),(-1,1),(1,1),(-1,-1))
+ldir = {'D':(1,0),'U':(-1,0),'R':(0,1),'L':(0,-1)}
+
+def transpose(mat):
+    return [list(col) for col in zip(*mat)]
+
+def matmul(L,B,MOD=(10**9 + 7)):
+    ans = [[0 for i in range(len(B[0]))] for j in range(len(L))]
+    for i in range(len(L)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                ans[i][j] = (ans[i][j]+L[i][k]*B[k][j])%MOD
+    return ans
+
+def matpow(M,power):
+    size = len(M)
+    result = [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+    while power:
+        if power % 2 == 1:
+            result = matmul(result, M)
+        M = matmul(M, M)
+        power //= 2
+    return result
+
+def gauss(A,mod=MOD):
+    m,n = len(A),len(A[0])-1
+    rank = 0
+    L = [-1]*n
+
+    for col in range(n):
+        for row in range(rank,m):
+            if A[row][col]:
+                A[rank],A[row] = A[row],A[rank]
+                break
+        else:
+            continue
+        k = pow(A[rank][col],-1,mod)
+        for j in range(col,n+1):
+            A[rank][j] = A[rank][j]*k%mod
+
+        for row in range(m):
+            if row!=rank and A[row][col]:
+                factor = A[row][col]
+                for j in range(col,n+1):
+                    A[row][j] -= factor*A[rank][j]
+                    A[row][j] %= mod
+        L[col] = rank
+        rank += 1
+    for row in range(rank, m):
+        if A[row][n]:
+            return None
+    return [A[L[i]][n]  if L[i]!=-1 else 0 for i in range(len(L))]
+
+def solve():
+    n,m,k = LII()
+    d = [[0]*n for i in range(n)]
+    for i in range(m):
+        u,v = LII_1()
+        d[u][v] += 1
+    print(matpow(d,k)[0][-1])
+    #L1 = LII()
+    #st = SI()
+solve()
